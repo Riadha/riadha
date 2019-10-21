78b7ec7877da9ccfbadcdc3cad742fe39fdc9fcf
KANGOGO\kango <kangvans@gmail.com>
2019-10-22 01:56:59 +0300

    model profile

---
+++ Riadha/Profile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Riadha;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'profiles';
+}
