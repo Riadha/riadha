956dbc970054142ba7d781171dc7bf15782af3b9
Bernard <bernard.nandwa@andela.com>
2019-10-22 19:44:17 +0300

    Setup the profile model (#7)
    * model profile
    
    * Move the profile class to a model folder.

---
+++ Riadha/Profiles/Data/Models/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Track an athlete profile
+ *
+ * @package Riadha\Profiles
+ */
+
+namespace Riadha\Profiles\Data\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'profiles';
+}
