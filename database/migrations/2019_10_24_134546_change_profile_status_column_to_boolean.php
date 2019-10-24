441ad3084b139fd0f176d05c43371f34030bfc40
Bernard Nandwa <bernard.nandwa@andela.com>
2019-10-24 16:54:00 +0300

    Change the profile status field to a boolean

---
+++ database/migrations/2019_10_24_134546_change_profile_status_column_to_boolean.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeProfileStatusColumnToBoolean extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->boolean('status')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+        });
+    }
+}
