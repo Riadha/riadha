5558864f3eaf1edf112e929f74116f39bc29bc26
Bernard Nandwa <bernard.nandwa@andela.com>
2019-11-13 08:37:57 +0300

    Add migrations to support profile photo

---
+++ database/migrations/2019_11_13_053359_add_profile_photo_to_profiles.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProfilePhotoToProfiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->string('profilephoto_file_name')->nullable();
+            $table->integer('profilephoto_file_size')->nullable();
+            $table->string('profilephoto_content_type')->nullable();
+            $table->timestamp('profilephoto_updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropColumn([
+                'profilephoto_file_name',
+                'profilephoto_file_size',
+                'profilephoto_content_type',
+                'profilephoto_updated_at'
+            ]);
+        });
+    }
+}
