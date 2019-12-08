6eec7edefe280ebe0852ea24c6c02f4cca0842e6
Bernard Nandwa <nandwabee@gmail.com>
2019-12-08 09:26:18 +0300

    Display profile photo

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,7 +7,12 @@
 @section('content')
     <div class="container profile-header-container bg-white">
         <div class="row profile-header-row">
-            <div class="col-12">
+            <div class="col-md-3 col-sm-12 col-lg-3 col-xl-3">
+                @if($profile->profilephoto_file_name)
+                    <img src="{{$profile->profilephoto_file_name->url('medium')}}" alt="Profile photo">
+                @endif
+            </div>
+            <div class="col-md-9 col-sm-12 col-lg-9 col-xl-9">
                 {{$profile->name}}
             </div>
         </div>
