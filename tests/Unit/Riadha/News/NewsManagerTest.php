3ee212c5b7668944219e7a4d8a6cdaa194e7c122
Bernard <r115codes@gmail.com>
2020-03-24 17:13:37 +0300

    Stop autodeployments from circleci.

--- tests/Unit/Riadha/News/NewsManagerTest.php
+++ tests/Unit/Riadha/News/NewsManagerTest.php
@@ -3,13 +3,23 @@
 namespace Tests\Unit\Riadha\News;
 
 
+use Riadha\News\NewsManager;
 use Tests\TestCase;
 
 class NewsManagerTest extends TestCase {
+    protected $newsManager;
+
+    protected function setUp(): void
+    {
+        $this->newsManager = new NewsManager();
+    }
+
     /**
      * The news manager class must exist
+     *
+     * @covers \Riadha\News\NewsManager::countNewsPosts
      */
-    public function testClassExistence(){
-        $this->assertTrue(true);
+    public function testNewsPostsCount(){
+        $this->assertIsNumeric($this->newsManager->countNewsPosts());
     }
 }
